39ccf4263e7c644bfee8bb77ee49a3a193832aca
Radcabna <radcabna@gmail.com>
2025-07-31 20:23:04 +0300

    Initial Commit

---
+++ Aviapilot/AviapilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AviapilotApp.swift
+//  Aviapilot
+//
+//  Created by Алкександр Степанов on 31.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct AviapilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
